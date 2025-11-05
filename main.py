e8fc092919ca9b786db9145a3c66c2d1206c0402
Nurbol <nurbolkachkynbekov47@gmail.com>
2025-11-05 19:58:14 +0600

    add flask

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-name = "Nurbol"
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello world"
+
+
+@app.route("/about_me")
+def about_me():
+    return "About me information"
+
+
+@app.route("/contact")
+def contact():
+    return "Nurbol email = nurbolkachkynbekov47@gmail.com"
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
